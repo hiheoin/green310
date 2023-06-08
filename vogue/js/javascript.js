6af87256134370c302b05542e4ea32bcaea80270
hiheoin <gjdlslo1@gmail.com>
2023-06-08 16:34:59 +0900

    0608

--- vogue/js/javascript.js
+++ vogue/js/javascript.js
@@ -10,7 +10,25 @@ $('#wrap em').click(function(){
     // 본인 스스로는 서서히 사라져라
     $(this).fadeOut('slow'); //숫자로 써도 됨 ex.800 대신 '' 없어도 됨
 
+});
+
+
+
+// gnb의 li를 클릭했을 때, 본인의 순번을 찾고 section의 div 순번에 맞추어 on값을 주어라
+
+$('.gnb li').click(function(){
+
+    let i = $(this).index()
+
+    $('section div').removeClass('on');
+    $('section div').eq(i).addClass('on');
+
+    $('#wrap nav').removeClass('on');
+    $('#wrap section').removeClass('on');
+    $('#wrap em').fadeIn();
+
 })
+
 
 
 
